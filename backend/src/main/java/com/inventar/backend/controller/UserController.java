cee43daace2fadde8717b97f58119347419f0190
Luka Lulić <148708883+ll54152@users.noreply.github.com>
2025-03-29 14:14:38 +0100

    Update UserController.java

--- backend/src/main/java/com/inventar/backend/controller/UserController.java
+++ backend/src/main/java/com/inventar/backend/controller/UserController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 //ToDo: Cors za testiranje
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
-@CrossOrigin(origins = "http://http://192.168.18.27:5173")
+@CrossOrigin(origins = {"http://localhost:5173", "http://http://192.168.18.27:5173"})
 @RequestMapping("/user")
 public class UserController {
 
